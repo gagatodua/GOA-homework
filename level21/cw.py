0e7c611a298002c8f19e60ddabe896d38cd775c4
gagatodua <gaga.todua90@gmail.com>
2025-12-12 02:05:20 -0800

    second commit

---
+++ level21/cw.py
@@ -0,0 +1,44 @@
+n1=5
+result = str(n1)
+print(result)
+print(type(result))
+
+
+
+car = ['mercedes benz']
+car.append(True)
+print(car)
+
+car = 'mercedes benz'
+result = car.upper()
+print(result)
+
+car = 'mercedes benz'
+result = car.lower()
+print(result)
+
+car = 'mercedes benz'
+result = car.capitalize()
+print(result)
+
+car = 'mercedes benz'
+result = car.upper()
+print(result)
+
+car = 'mercedes benz'
+result = car.upper()
+print(result)
+
+car = 'mercedes benz'
+result = car.find()
+print(result)
+
+
+car = 'mercedes benz'
+result = car.type()
+print(result)
+
+
+car = 'mercedes benz'
+result = car.remove()
+print(result)
---
+++ level21/hw.py
@@ -0,0 +1,135 @@
+book = 'bible'
+book.find('bible')
+print(book)
+
+
+car = 'mercedes benz'
+car.find('mercedes benz')
+print(car)
+
+
+model = 'adriana lima'
+model.find('adriana lima')
+print(model)
+
+
+
+
+book = ['bible']
+book.append(True)
+print(book)
+
+
+car = ['mercedes benz']
+car.append(True)
+print(car)
+
+
+model = ['adriana lima']
+model.append(True)
+print(model)
+
+
+
+
+
+book = 'bible'
+result = book.upper()
+print(result)
+
+
+car = 'mercedes benz'
+result = car.upper()
+print(result)
+
+
+model = 'adriana lima'
+result = model.upper()
+print(result)
+
+
+
+
+
+book = 'bible'
+result = book.lower()
+print(result)
+
+
+car = 'mercedes benz'
+result = car.lower()
+print(result)
+
+
+model = 'adriana lima'
+result = model.lower()
+print(result)
+
+
+
+
+
+book = 'bible'
+result = book.capitalize()
+print(result)
+
+
+car = 'mercedes benz'
+result = car.capitalize()
+print(result)
+
+
+model = 'adriana lima'
+result = model.capitalize()
+print(result)
+
+
+
+
+
+book = ['bible']
+book.remove('bible')
+print(car)
+
+
+car = ['mercedes benz']
+car.remove('mercedes benz')
+print(car)
+
+
+model = ['adriana lima']
+model.remove('adriana lima')
+print(model)
+
+
+
+
+car='mercedes benz'
+result = str(car)
+print(result)
+print(type(result))
+
+
+
+
+model='adriana lima'
+result = str(model)
+print(result)
+print(type(result))
+
+
+
+
+book='bible'
+result = str(book)
+print(result)
+print(type(result))
+
+
+
+
+
+
+listen = [1,2,3,4,5,6,7,8]
+print(min(listen))
+print(max(listen))
